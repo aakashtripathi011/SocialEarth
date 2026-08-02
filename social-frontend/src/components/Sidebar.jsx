70036c6b5eb45fe76a0470013847ae60185f1656
aakashtripathi011 <aakashtripathi914@gmail.com>
2026-08-02 14:53:17 +0530

    Set up feed page and sidebar component

---
+++ social-frontend/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h1>SocialEarth</h1>
+      <button>Home</button>
+      <button>Search</button>
+      <button>Create</button>
+      <button>Notifications</button>
+      <button>Messages</button>
+      <button>Profile</button>
+      <button>Settings</button>
+    </div>
+  );
+}
+export default Sidebar;
---
+++ social-frontend/src/pages/Feed.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../components/Sidebar";
+
+function Feed() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Feed;
