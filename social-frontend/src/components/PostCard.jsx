89fff1b130872206d58b81c8c83da03107b63d13
aakashtripathi011 <aakashtripathi914@gmail.com>
2026-08-02 18:46:29 +0530

    Add PostCard component with header and bottom actions

--- social-frontend/src/components/PostCard.jsx
+++ social-frontend/src/components/PostCard.jsx
@@ -1,5 +1,26 @@
+import "../styles/PostCard.css";
+import { Heart, MessageCircle, Send } from "react-feather";
+
 function PostCard() {
-  return <div>Post Card</div>;
+  return (
+    <div className="post-card">
+      <div className="post-card-header">
+        <h1>UserName</h1>
+        <button className="alter-post">...</button>
+      </div>
+      <div className="bottom">
+        <button className="like-btn">
+          <Heart color="white" size={23} strokeWidth={1.5} />
+        </button>
+        <button className="comment-btn">
+          <MessageCircle color="white" size={23} strokeWidth={1.5} />
+        </button>
+        <button className="share-btn">
+          <Send color="white" size={21} strokeWidth={1.5} />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default PostCard;
